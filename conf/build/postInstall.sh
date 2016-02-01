1de0da96b13c758101cbb7c4769cda538b2bc2c7
enzo_figini <enzo.figini@italtel.com>
2016-02-01 14:18:44 +0100

    Add rpm pre/post install script. They do automatically links for start server when linux start.

---
+++ conf/build/postInstall.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo "post install"
+
+[ -d /etc/rc0.d ] && ln -s /etc/init.d/nfs /etc/rc0.d/K01nfs 
+[ -d /etc/rc1.d ] && ln -s /etc/init.d/nfs /etc/rc1.d/S99nfs
+[ -d /etc/rc2.d ] && ln -s /etc/init.d/nfs /etc/rc2.d/S99nfs
+[ -d /etc/rc3.d ] && ln -s /etc/init.d/nfs /etc/rc3.d/S99nfs
+[ -d /etc/rc4.d ] && ln -s /etc/init.d/nfs /etc/rc4.d/S99nfs
+[ -d /etc/rc5.d ] && ln -s /etc/init.d/nfs /etc/rc5.d/S99nfs
+[ -d /etc/rc6.d ] && ln -s /etc/init.d/nfs /etc/rc6.d/K01nfs
---
+++ conf/build/postUninstall.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+echo "post Uninstall"
+
+rm /etc/rc*.d/[KS]*nfs
